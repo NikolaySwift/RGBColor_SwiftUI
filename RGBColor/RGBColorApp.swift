88e5647a9afbe46e1138cecea12fe5dd0ed9406d
NikolayD <volodar86@yandex.ru>
2024-09-11 09:04:01 +0300

    Initial Commit

---
+++ RGBColor/RGBColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBColorApp.swift
+//  RGBColor
+//
+//  Created by NikolayD on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct RGBColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
